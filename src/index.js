e134ec0f57e2e6d13610d9c2bbbe0a7c02558962
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-03-24 16:04:21 +0300

    Modify the index page to accept and list meals from API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
-// import _ from 'lodash';
 import './style.css';
+import getMeals from './modules/getMeals.js';
 
-function component() {
-  const element = document.createElement('div');
+const [liStarter, liVegan, livege] = document.querySelectorAll('li');
+let selectedli = liStarter;
 
-  // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.classList.add('hello');
-  return element;
-}
+const allStarter = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Starter';
+const allvegan = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan';
+const allvege = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian';
 
-document.body.appendChild(component());
+liStarter.addEventListener('click', () => {
+  selectedli = liStarter;
+  liStarter.classList.add('selected');
+  liVegan.classList.remove('selected');
+  livege.classList.remove('selected');
+  selectedli.textContent = 'Starter';
+  livege.textContent = 'Vegeterian';
+  liVegan.textContent = 'Vegan';
+  getMeals(allStarter);
+});
+
+liVegan.addEventListener('click', () => {
+  selectedli = liStarter;
+  liStarter.classList.remove('selected');
+  liVegan.classList.add('selected');
+  livege.classList.remove('selected');
+  selectedli.textContent = 'Vegan';
+  liStarter.textContent = 'Starter';
+  livege.textContent = 'Vegeterian';
+  getMeals(allvegan);
+});
+
+livege.addEventListener('click', () => {
+  selectedli = liStarter;
+  liStarter.classList.remove('selected');
+  liVegan.classList.remove('selected');
+  livege.classList.add('selected');
+  selectedli.textContent = 'Vegeterian';
+  liStarter.textContent = 'Starter';
+  liVegan.textContent = 'Vegan';
+  getMeals(allvege);
+});
+
+getMeals(allStarter);
