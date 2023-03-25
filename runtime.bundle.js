e215c47a0cc38c8d51e982858f39dc582cf28f2c
yosaddis <60548252+yosaddis@users.noreply.github.com>
2023-03-26 02:13:34 +0300

    Updates

--- runtime.bundle.js
+++ runtime.bundle.js
@@ -163,7 +163,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_demo"] = self["webpackChunkwebpack_demo"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunkMealDB"] = self["webpackChunkMealDB"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
